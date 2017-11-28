65988ab679646c1fb3a0ab3c1c402c0a43508b52
damienepsztein <damiepsz3@gmail.com>
2017-11-28 14:05:41 -0500

    fix: :hammer: refetch query in proper way

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -9,9 +9,9 @@ import query from '../queries/fetchSongs'
 class SongList extends Component {
   onSongDelete(id) {
     this.props.mutate({
-      variables: { id },
-      refetchQueries: [{ query }]
+      variables: { id }
     })
+      .then(() => this.props.data.refetch())
   }
 
   renderSongs() {
